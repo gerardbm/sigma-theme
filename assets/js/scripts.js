1857f367332fa93e039ec5e9130287d503276344
Gerard <gerardbajona@gmail.com>
2019-09-07 00:47:11 +0200

    Add the scroll up button

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,36 @@
-(function() {
-	"use strict";
-	window.onscroll = function() {stickyMenu()};
+window.onscroll = function() { stickyMenu(); scrollToTop(); }
 
-	function stickyMenu() {
-		var menu = document.getElementsByClassName("sticky")[0];
-		var sticky = menu.offsetTop;
-		if (window.pageYOffset > sticky) {
-			menu.classList.add("fixed-menu");
-		} else {
-			menu.classList.remove("fixed-menu");
-		}
-		console.log(sticky);
+function stickyMenu() {
+	var menu = document.getElementsByClassName("sticky")[0];
+	var sticky = menu.offsetTop;
+	if (window.pageYOffset > sticky) {
+		menu.classList.add("fixed-menu");
+	} else {
+		menu.classList.remove("fixed-menu");
 	}
-})();
+}
+
+var button = document.getElementsByClassName("scrollup")[0];
+
+function scrollToTop() {
+	if (window.pageYOffset > 250) {
+		button.style.display = "block";
+	} else {
+		button.style.display = "none";
+	}
+}
+
+function topFunction(scrollDuration) {
+	var cosParameter = window.scrollY / 2,
+		scrollCount = 0,
+		oldTimestamp = performance.now();
+	function step (newTimestamp) {
+		scrollCount += Math.PI / (scrollDuration / (newTimestamp - oldTimestamp));
+		if (scrollCount >= Math.PI) window.scrollTo(0, 0);
+		if (window.scrollY === 0) return;
+		window.scrollTo(0, Math.round(cosParameter + cosParameter * Math.cos(scrollCount)));
+		oldTimestamp = newTimestamp;
+		window.requestAnimationFrame(step);
+	}
+	window.requestAnimationFrame(step);
+}
