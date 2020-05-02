cec9e83a17de1e8a35754412e44bfa1b3b708b81
Gerard <gerardbajona@gmail.com>
2020-05-03 01:16:52 +0200

    Fix a bug in the scroll to top function, under IE

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -23,13 +23,13 @@ function scrollToTop() {
 }
 
 function topFunction(scrollDuration) {
-	var cosParameter = window.scrollY / 2,
+	var cosParameter = window.pageYOffset / 2,
 		scrollCount = 0,
 		oldTimestamp = performance.now();
 	function step (newTimestamp) {
 		scrollCount += Math.PI / (scrollDuration / (newTimestamp - oldTimestamp));
 		if (scrollCount >= Math.PI) window.scrollTo(0, 0);
-		if (window.scrollY === 0) return;
+		if (window.pageYOffset === 0) return;
 		window.scrollTo(0, Math.round(cosParameter + cosParameter * Math.cos(scrollCount)));
 		oldTimestamp = newTimestamp;
 		window.requestAnimationFrame(step);
